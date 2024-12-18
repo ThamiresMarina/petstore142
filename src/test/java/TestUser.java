ca4a60950f27f9f77d55445c874bebf6bf8781c5
ThamiresMarina <thamiresmarina@gmail.com>
2024-12-18 18:58:30 +0000

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,36 @@
+// 0 nome do pacote
+
+// 1 biblioteca
+
+// 2 classe
+
+import org.junit.jupiter.api.Test;
+
+public class TestUser {
+    // 2.1 atributos
+    static String ct = "application/jason"; // content-type
+    static String uriUser = " Base URL: petstore.swagger.io/v2/user";
+                
+
+    // 2.2 funções e métodos
+    // 2.2.1 funções e métodos comuns / úteis
+
+    // Função de leitura de Json
+    public static String lerArquivoJson(String arquivoJson){
+        //TODO:"Completar a leitura do aqruivo"
+        //return new String(Files)
+        return null;
+    }
+
+    //2.2.2 métodos de teste
+    @Test
+    public void TestPostPet(){
+        //carregar os dasdos do arquivo json do pet
+        //TODO: criar o método post
+        // feito pela IA - String jsonBody = new String(Files.readAllBytes(Paths.get("path/to/your/json/file.json")), StandardCharsets.UTF_8);
+    }
+
+
+    
+    
+}
