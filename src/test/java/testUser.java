5971d2311bd5a678f222a1a598876722b8e05b4b
ThamiresMarina <thamiresmarina@gmail.com>
2025-04-02 17:59:50 +0100

    Initial commit - PetStore API testing

---
+++ src/test/java/testUser.java
@@ -0,0 +1,48 @@
+import static org.hamcrest.Matchers.containsString;   
+import static org.hamcrest.Matchers.hasLength;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
+
+public class testUser {
+    String ct ="application/json"; // content-type
+    String uriUser = "https://petstore.swagger.io/v2/user";
+
+    @Test 
+    public void testLogin(){
+        //configura 
+        String userName = "robert";
+        String password = "abcdef";
+
+        String resultadoEsperado = "logged in user session:";
+
+        Response resposta = (Response) given()
+            .contentType(ct) // tipo de conteúdo é
+            .log().all() //exibir tudo na ida
+            
+        //executa
+        .when() //quando
+            .get(uriUser + "/login?username=" + userName + "&password=" + password)//envie o corpo da requisição
+
+        //valida
+        .then()
+            .log().all() //exibir tudo na volta
+            .statusCode(200)
+            .body("code", Is.is(200)) //verifica se o código é 200
+            .body("type", Is.is("unknown")) //verifica se o tipo é unknown
+            .body("message", containsString(resultadoEsperado)) //verifica se a mensagem é igual ao esperado
+            .body("message", hasLength(36)) //tamanho do campo message
+        .extract()
+        ;        
+
+        //extração
+        String token = resposta.jsonPath().getString("message").substring(23); //extrai o token da resposta
+        System.out.println("Conteúdo do Token: " + token); //exibe o token no console
+        
+        
+    }   
+
+
+}
